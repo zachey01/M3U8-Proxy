f3fdf5656d8c327ece266e72e3f5ec367c91207c
qwaq <63107653+zachey01@users.noreply.github.com>
2026-01-09 15:04:25 +0500

    Update server.ts

--- src/libraries/server.ts
+++ src/libraries/server.ts
@@ -433,7 +433,7 @@ function createServer(options) {
 
 const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 8080;
-const web_server_url = process.env.PUBLIC_URL || `http://${host}:${port}`;
+const web_server_url = process.env.PUBLIC_URL;
 
 export default function server() {
     const originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
